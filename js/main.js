c5c3d87a8955c76775cb7beceadc485fe20aa701
snehareddy048 <sneha.yeluguri@capitalone.com>
2014-11-25 17:57:16 -0500

    creating a view class

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 var app = {
 
-    findByName: function() {
-          var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-    },
-    renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
+    
 showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
@@ -20,7 +11,8 @@ showAlert: function (message, title) {
     initialize: function() {
     var self = this;
     this.store = new MemoryStore(function() {
-        self.renderHomeView();
+         $('body').html(new HomeView(self.store).render().el);
+        
     });
     this.homeTpl = Handlebars.compile($("#home-tpl").html());
 this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
