ec515112675f0ca7b74989706589550d85e2351b
snehareddy048 <sneha.yeluguri@capitalone.com>
2014-11-25 17:04:09 -0500

    single page application

--- js/main.js
+++ js/main.js
@@ -12,6 +12,16 @@ var app = {
             }
         });
     },
+    renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+},
 showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
@@ -22,11 +32,9 @@ showAlert: function (message, title) {
     initialize: function() {
     var self = this;
     this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
+        self.renderHomeView();
     });
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
 }
-
 };
 
 app.initialize();
