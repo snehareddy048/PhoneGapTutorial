848c9a3f943132d5f543d1d81ea68d55d3363274
snehareddy048 <sneha.yeluguri@capitalone.com>
2014-11-25 15:56:41 -0500

    creating a view class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 var app = {
 
-    findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-},
+    
 showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
@@ -13,18 +8,13 @@ showAlert: function (message, title) {
         alert(title ? (title + ": " + message) : message);
     }
 },
-renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
+
     initialize: function() {
        var self = this;
     this.store = new MemoryStore(function() {
-        self.renderHomeView();
-    }); 
-    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    }
+        $('body').html(new HomeView(self.store).render().el);
+    });
+       }
 
 
 };
